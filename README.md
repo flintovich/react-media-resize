ea12ddf97d6c3ae02e083dc1f095a6783b4085e0
Ruslan Sukhorukov <flintovich@gmail.com>
2017-06-17 23:16:27 +0300

    added all files

---
+++ README.md
@@ -0,0 +1,97 @@
+# react-media-resize
+Adaptive React components with a callback on entering or leaving in particular browser width on resize.
+
+## Install
+
+```console
+$ npm install --save react-media-resize
+```
+
+## API
+
+### `<MediaRange />`
+The component that present possibility to work with responsive design in ReactJS.
+It let us to use different components and change component state witch depends from browser screen width (on window resize).
+
+#### Props
+
+- `range` (*String or array*): Range with width or array of ranges. 
+- `onEnter` (*Function*): Callback for entering in particular browser width
+- `onLeave` (*Function*): Callback for leaving from particular browser width
+
+## Component Usage
+```javascript
+import React from 'react';
+import MediaRange from 'react-media-resize';
+
+export default class App extends React.Component {
+    handlerEnterWidth() {
+        console.log('You entered in range 768..1024');
+    }
+    
+    handlerLeaveWidth() {
+        console.log('You left range 768..1024');
+    }
+    
+    render(
+        <MediaRange
+          range="768..1024"
+          onEnter={this.handlerEnterWidth}
+          onLeave={this.handlerLeaveWidth}
+         >
+           <AnotherComponent />
+        </MediaRange>
+    )
+}
+```
+
+You can also use several ranges for different window sizes with own callbacks:
+```javascript
+import React from 'react';
+import MediaRange from 'react-media-resize';
+
+export default class App extends React.Component {
+    render() {
+        const rangesList = [
+            {
+                range: '..480',
+                onEnter: () => {
+                  console.log('Entered in mobile width');
+                }
+            },
+            {
+                range: '481..1024',
+                onEnter: () => {
+                  console.log('Entered in tablet width');
+                },
+                onLeave: () => {
+                  console.log('Left tablet width');
+                }
+            },
+            {
+                range: '1025..',
+                onEnter: () => {
+                  console.log('Entered in desctop screen width');
+                }
+            }
+        ];
+    
+        return (
+            <MediaRange range={rangesList}>
+               <AnotherComponent />
+            </MediaRange>
+        )
+    }
+}
+```
+
+Also we can include to render or exclude component on window resize:
+```javascript
+<MediaRange>
+   <AnotherComponent range="..479" />
+   <AnotherComponent range="480..1200" />
+   <AnotherComponent range="480..960" />
+   <AlwaysVisibleComponent />
+   <AnotherComponent range="1200.." />
+</MediaRange>
+```
---
+++ src/MediaRangeHelper.js
@@ -0,0 +1,117 @@
+var ResponsiveHelper = (function(){
+  // init variables
+  var handlers = [],
+    prevWinWidth,
+    nativeMatchMedia = false;
+
+  // detect match media support
+  if(window.matchMedia) {
+    if(window.Window && window.matchMedia === Window.prototype.matchMedia) {
+      nativeMatchMedia = true;
+    } else if(window.matchMedia.toString().indexOf('native') > -1) {
+      nativeMatchMedia = true;
+    }
+  }
+
+  // prepare resize handler
+  function resizeHandler() {
+    var winWidth = window.innerWidth;
+    if(winWidth !== prevWinWidth) {
+      prevWinWidth = winWidth;
+
+      // loop through range groups
+      handlers.forEach(function(rangeObject) {
+        var rangeData = rangeObject.data;
+
+        for (var key in rangeData) {
+          var item = rangeData[key];
+
+          // disable current active area if needed
+          if(item.currentActive && !matchRange(item.range[0], item.range[1])) {
+            item.currentActive = false;
+            if(typeof item.disableCallback === 'function') {
+              item.disableCallback(item.rangeString);
+            }
+          }
+
+          // enable areas that match current width
+          if(!item.currentActive && matchRange(item.range[0], item.range[1])) {
+            item.currentActive = true;
+            if(typeof item.enableCallback === 'function') {
+              item.enableCallback(item.rangeString);
+            }
+          }
+        }
+      });
+    }
+  }
+
+  // test range
+  function matchRange(r1, r2) {
+    var mediaQueryString = '';
+    if(r1 > 0) {
+      mediaQueryString += '(min-width: ' + r1 + 'px)';
+    }
+    if(r2 < Infinity) {
+      mediaQueryString += (mediaQueryString ? ' and ' : '') + '(max-width: ' + r2 + 'px)';
+    }
+    return matchQuery(mediaQueryString, r1, r2);
+  }
+
+  // media query function
+  function matchQuery(query, r1, r2) {
+    if(window.matchMedia && nativeMatchMedia) {
+      return matchMedia(query).matches;
+    } else if(window.styleMedia) {
+      return styleMedia.matchMedium(query);
+    } else if(window.media) {
+      return media.matchMedium(query);
+    } else {
+      return prevWinWidth >= r1 && prevWinWidth <= r2;
+    }
+  }
+
+  // range parser
+  function parseRange(rangeStr) {
+    var rangeData = rangeStr.split('..');
+    var x1 = parseInt(rangeData[0], 10) || -Infinity;
+    var x2 = parseInt(rangeData[1], 10) || Infinity;
+    return [x1, x2].sort(function(a, b){
+      return a - b;
+    });
+  }
+
+  // clear resize handlers
+  function clearHandlers() {
+    handlers = [];
+  }
+
+  // export public functions
+  return {
+    addRange: function(ranges) {
+      // parse data and add items to collection
+      var result = {data:{}};
+
+      for(var key in ranges) {
+        var data = ranges[key];
+
+        result.data[key] = {
+          range: parseRange(key),
+          rangeString: key,
+          enableCallback: data.on,
+          disableCallback: data.off
+        };
+      }
+
+      handlers.push(result);
+
+      // call resizeHandler to recalculate all events
+      prevWinWidth = null;
+      resizeHandler();
+    },
+    resizeHandler: resizeHandler,
+    clearHandlers: clearHandlers
+  };
+}());
+
+module.exports = ResponsiveHelper;
